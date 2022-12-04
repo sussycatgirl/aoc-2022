d558b302f5c4305b26c23344ec67a7ef6087ad2a
Lea <me@janderedev.xyz>
2022-12-04 09:52:31 +0100

    Day 4 Part 1 and 2

---
+++ Day 4/part_1.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input')
+    .toString('utf8')
+    .trim()
+    .split('\n')
+    .map(line => line.split(',').map(pair => pair.split('-').map(i => Number(i))));
+
+let result = 0;
+
+for (const pair of input) {
+    if ((pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1]) ||
+        (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]))
+    {
+        result++;
+    }
+}
+
+console.log(result);
