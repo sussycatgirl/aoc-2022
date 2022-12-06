7fa33bebc5502ae6e0e896e622dfa3fbdfe6de59
Lea <me@janderedev.xyz>
2022-12-06 10:02:12 +0100

    Rewrite day 6 in rust

---
+++ Day 6/rust/part_2/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let input = include_str!("../../../input")
+        .trim_end()
+        .chars();
+
+    let mut buf: Vec<char> = Vec::new();
+
+    for (i, item) in input.enumerate() {
+        buf.push(item);
+        if buf.len() > 14 {
+            buf.remove(0);
+
+            let mut tmp_buf = buf.clone();
+            tmp_buf.sort();
+            tmp_buf.dedup();
+
+            if buf.len() == tmp_buf.len() {
+                println!("{}", i + 1);
+                return;
+            }
+        }
+    }
+}
